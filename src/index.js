30b1df085293e6eedc67d98502d46952bdc4e7ef
Jhordan S <jhordan.kendy@outlook.com>
2024-02-01 20:44:18 -0300

    add db map

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ function route(target, { kind, name }) {
     return target
 }
 
+const { once } = require('events')
 const { createServer } = require('http')
+const { randomUUID } = require('crypto')
+const Db = new Map()
 
 @route
 class Server {
     static async handler(req, res) {
-        return res.end('ok')
+        if(req.method === "POST"){
+            const data = await once(req,'data')
+            const item = jSON.parse(data)
+            item.id = randomUUID()
+
+            Db.set(item.id, item)
+            res.writeHead(201)
+            res.end(JSON.stringify({message: item}))
+            return
+        }
+
+        res.writeHead(200)
+        res.end(JSON.stringify({message: [...Db.values]}))
     }
 }
 
